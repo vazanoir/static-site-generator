2bdb207348cf78cf69c782a13a3bef197913daee
Vazanoir <romain.gianolio@proton.me>
2024-09-26 15:40:33 +0200

    clean main.py

--- src/main.py
+++ src/main.py
@@ -1,11 +1,5 @@
-from textnode import TextNode
-from html import text_node_to_html_node
-
-
 def main():
-    node = TextNode("this is an image of a fox", "image", "https://boot.dev/fox-image.png")
-    leaf = text_node_to_html_node(node)
-    print(leaf.to_html())
+    print("hello world")
 
 
 if __name__ == "__main__":
