7ba91e0d903c7bd5eb187f25e0de21afe4d7cf80
Vazanoir <romain.gianolio@proton.me>
2024-10-01 15:16:16 +0200

    fix line too long

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -6,7 +6,10 @@ class HTMLNode():
         self.props = props
 
     def __repr__(self):
-        return f"HTMLNode ({self.tag}, {self.value}, {self.children}, {self.props})"
+        return (
+            f"HTMLNode ({self.tag}, {self.value}, "
+            "{self.children}, {self.props})"
+        )
 
     def to_html(self):
         raise NotImplementedError()
