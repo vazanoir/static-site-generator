f980e3e37767dabb911d5a230adfb77761ebc9e5
Vazanoir <romain.gianolio@proton.me>
2024-10-01 17:09:23 +0200

    add recursive copy of static to public

--- src/main.py
+++ src/main.py
@@ -1,11 +1,29 @@
-from shutil import rmtree
-from os import mkdir
+from shutil import rmtree, copy
+from os import mkdir, listdir, path
 
 
 def main():
-    public = "public"
-    rmtree(public)
-    mkdir(public)
+    remove_dir_content("public")
+    copy_tree("static", "public")
+
+
+def copy_tree(source, destination):
+    ls = listdir(source)
+
+    for item in ls:
+        source_item = path.join(source, item)
+        destination_item = path.join(destination, item)
+
+        if path.isfile(source_item):
+            copy(source_item, destination_item)
+        else:
+            mkdir(destination_item)
+            copy_tree(source_item, destination_item)
+
+
+def remove_dir_content(dir):
+    rmtree(dir)
+    mkdir(dir)
 
 
 if __name__ == "__main__":
